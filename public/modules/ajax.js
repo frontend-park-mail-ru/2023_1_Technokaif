01574770bc411528a57b25507af59004ec8334ed
Ilya <2002marsic@gmail.com>
2023-03-05 14:51:14 +0300

    changed logout

--- public/modules/ajax.js
+++ public/modules/ajax.js
@@ -34,7 +34,8 @@ export class Ajax {
                     method,
                     credentials: 'include',
                     headers: {
-                        'content-type': 'application/json;'
+                        'content-type': 'application/json;',
+                        'Authorization': `Bearer ${localStorage.getItem('jwt')}`
                     }
                 });
         } else {
@@ -49,9 +50,9 @@ export class Ajax {
                 });
         }
 
-        if (localStorage.getItem('jwt') != null && method === AJAX_METHODS.GET) {
-            request.headers.Authorization = 'Bearer ' + localStorage.getItem('jwt');
-        }
+        // if (localStorage.getItem('jwt') != null && method === AJAX_METHODS.GET) {
+        //      =
+        // }
 
         fetch(request).then(
             responseRaw => responseRaw.json().then(
