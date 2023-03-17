cabe0eeb5c5d5191eb104124640c2732c5dd8917
KolobokMysnoy <Inkasator200@yandex.ru>
2023-03-17 22:57:52 +0300

    Precommit test for eslint and webpack build

--- public/components/TracksComp/convert.js
+++ public/components/TracksComp/convert.js
@@ -1,6 +1,10 @@
 import { convertImgSrc } from '../../utils/functions/stringOperations';
 
 // todo jsdoc
+/**
+ *
+ * @param {*} options
+ */
 function convert(options) {
     convertImgSrc(options.fn(this));
 }
