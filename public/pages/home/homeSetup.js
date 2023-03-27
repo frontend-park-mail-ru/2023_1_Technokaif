c3b763f84b41da0ba1f05b4d4ad67b843d9d9ca8
KolobokMysnoy <Inkasator200@yandex.ru>
2023-03-27 08:59:40 +0300

    Add tape

--- public/pages/home/homeSetup.js
+++ public/pages/home/homeSetup.js
@@ -10,43 +10,29 @@ export function homeSetup(items) {
         tracksTitle: 'tracks-title',
         tracksFullList: 'tape__show-text',
 
-        tracksDiv: 'tape__components',
+        coverMainClass: 'tape__components',
         trackDiv: 'track',
-        trackImgDiv: 'track-img',
-        trackImg: 'track__img',
-        trackNameDiv: 'track__name',
+        imgDiv: 'track-img',
+        imgClass: 'track__img',
+        titleTextDiv: 'track__name',
         trackIdDiv: 'track-id',
 
-        innerArtistsDiv: 'inner-artists',
-        innerArtistNameDiv: 'track__artist-name',
+        footerMainDiv: 'inner-artists',
+        footerElementDiv: 'track__artist-name',
         innerArtistIdDiv: null,
 
         artistsTitleDiv: 'tape__title',
         artistsTitle: 'artists-title',
         artistsFullList: 'tape__show-text',
 
-        artistsDiv: 'tape__components',
         artistDiv: 'artist',
-        artistImgDiv: 'artist-img',
-        artistImg: 'artist__img',
-        artistNameDiv: 'artist__name',
         artistIdDiv: 'artist-id',
 
-        albumsTitleDiv: 'tape__title',
         albumsTitle: 'albums-title',
         albumsFullList: 'tape__show-text',
 
-        albumsDiv: 'tape__components',
         albumDiv: 'album',
-        albumImgDiv: 'album-img',
-        albumImg: 'album__img',
-        albumNameDiv: 'album__name',
         albumIdDiv: 'album-id',
-        albumDescriptionDiv: 'album__description',
-
-        defaultAlbumCover: '/static/img/albums/gorgorod.jpg',
-        defaultTrackCover: '/static/img/tracks/pink.jpeg',
-        defaultArtistCover: '/static/img/artists/singer.jpg',
 
         content: items,
     });
