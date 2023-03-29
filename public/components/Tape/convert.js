bd0891a362646bc55c34d1cd15159e84f007d53a
Ilya <2002marsic@gmail.com>
2023-03-29 19:30:13 +0300

    names added

--- public/components/Tape/convert.js
+++ public/components/Tape/convert.js
@@ -1,8 +1,8 @@
 import { convertImgSrc } from '../../utils/functions/stringOperations.js';
 
-// todo jsdoc
 /**
- *
+ * Function to convert srcImg to static format.
+ * @param(*) options.
  */
 export default function convert(options) {
     return convertImgSrc(options.fn(this));
