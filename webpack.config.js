755fba576adc15fdf14ba6c3483c2e727cbb17f5
KolobokMysnoy <Inkasator200@yandex.ru>
2023-04-09 00:49:59 +0300

    Add babel

--- webpack.config.js
+++ webpack.config.js
@@ -49,6 +49,18 @@ module.exports = {
     module: {
         rules: [
             {
+                test: /\.(?:js|mjs|cjs)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            ['@babel/preset-env', { targets: 'defaults' }],
+                        ],
+                    },
+                },
+            },
+            {
                 test: /\.handlebars$/,
                 loader: 'handlebars-loader',
             },
